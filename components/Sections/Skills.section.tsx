0261b31f3d99a2f9d43b2d4c6ee0a1bc9aed4312
Robert Wearden <rwearden@LT-VUE-439.local>
2022-05-27 20:41:02 +0100

    :tada: feat(pill): fixed typo

--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
           <span className="text-white">React JS</span>
-          &nbsp;as my Frontend Framework
+          &nbsp;as my frontend framework
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
@@ -52,6 +52,5 @@ const Skills = () => {
     </div>
   );
 };
-
 
 export default Skills;
