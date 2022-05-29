335ec6c993371f63cf5bf3b198dce83e8778caef
Robert Wearden <rwearden@LT-VUE-439.local>
2022-05-29 21:05:42 +0100

    :tada: feat(pill): added more skills to Skills section

--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -15,38 +15,42 @@ const Skills = () => {
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
           <span className="text-white">JavaScript </span>
-          &nbsp;as my main frontend language
+          &nbsp;as my frontend language
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Material UI and Styled Components </span>
-          &nbsp;as my CSS frameworks
+          <span className="text-white">Material UI</span>
+          &nbsp;as my CSS framework
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
           <span className="text-white">Python </span>
-          &nbsp;as my main backend language
+          &nbsp;as my backend language
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Flask and Fast API </span>
-          &nbsp;as my API frameworks
+          <span className="text-white">Flask</span>
+          &nbsp;as my API framework
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Docker, AWS and Kubernetes </span>
+          <span className="text-white">AWS and k8s</span>
           &nbsp;as my CI/CD pipeline
         </p>
       </div>
 
       <p className="text-lg font-medium text-slate-300">
         ...more skills include <span className="text-white">Node JS</span>,{" "}
+        <span className="text-white">Docker</span>,{" "}
         <span className="text-white">React Native</span>,{" "}
-        <span className="text-white">PostreSQL and MySQL</span>,{" "}
+        <span className="text-white">Stlyed Components</span>,{" "}
+        <span className="text-white">Fast API</span>,{" "}
+        <span className="text-white">PostreSQL</span>,{" "}
+        <span className="text-white">MySQL</span>,{" "}
         <span className="text-white">Azure Data Studio</span>{" "}
       </p>
     </div>
